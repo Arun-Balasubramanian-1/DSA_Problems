6c7415a6f5e5f3db0b036ec795a51a53a150f8e2
Arun-Balasubramanian-1 <arun.balasubramanian.97@gmail.com>
2025-01-03 07:46:48 +0530

    Bubble Sort

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+// Swap the adjacent element and move the largest value to the last after each iteration
+
+public class BubbleSort {
+  public static int[] bubbleSort(int[] arr){
+
+    for(int i=0; i< arr.length; i++){
+
+      for(int j=1; j<arr.length-i; j++){
+        
+        if(arr[j-1] > arr[j]){
+          int temp = arr[j-1];
+          arr[j-1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+      
+    }
+    return arr;
+  }
+
+  public static void main(String[] args){
+    int[] arr = {7, 4, 1, -2, 5, 3};
+    arr = bubbleSort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+}
